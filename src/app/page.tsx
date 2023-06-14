24a9fb9d61261355d5679c42bb0d62bda0d9a3bb
justinpsiegel <justinpsiegel@gmail.com>
2023-06-14 14:26:52 -0400

    Added new todo functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,16 @@ function getTodos() {
   return prisma.todo.findMany();
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+
+  console.log(id, complete);
+}
+
 export default async function Home() {
-  const todos = await prisma.todo.findMany();
-  //await prisma.todo.create({ data: { title: "test", complete: false } });
+  const todos = await getTodos();
 
   return (
     <>
@@ -23,7 +30,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
